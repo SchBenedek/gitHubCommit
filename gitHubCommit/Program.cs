788a5d7744ae677c681ce7f901f823f129e7e8d5
Benedek Schlesser <schlesser.benedek@petrik.hu>
2023-12-18 12:25:59 +0100

    min --> max érték

--- gitHubCommit/Program.cs
+++ gitHubCommit/Program.cs
@@ -10,6 +10,32 @@ namespace gitHubCommit
     {
         static void Main(string[] args)
         {
-        }
+            List<int>lista= new List<int>();
+            Console.WriteLine("Egy számot: ");
+            int a=Convert.ToInt32(Console.ReadLine());
+            string b = "";
+            while (a!=0)
+            {
+                b = "";
+                foreach(var item in lista)
+                {
+                    b += (item+", ");
+                }
+                Console.WriteLine(b);
+                Console.WriteLine("Egy számot: ");
+                a=Convert.ToInt32(Console.ReadLine());
+                lista.Add(a);
+            }
+            int c = 0;
+            foreach(var item in lista)
+            {
+                if (item > c)
+                {
+                    c = item;
+                }
+            }
+            Console.WriteLine("legnagyobb: "+c);
+            Console.ReadKey();
+           }
     }
 }
